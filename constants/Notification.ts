498a93d6f28928c84977c49b5451991ea2dc2d60
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-05-28 23:47:48 +0700

    add notify time and style notification screen

--- constants/Notification.ts
+++ constants/Notification.ts
@@ -1,20 +1,20 @@
-const today = new Date()
-const yesterday = new Date(today.getDate() - 1)
-const beforeYesterday = new Date(today.getDate() - 2)
-
 export const NotifyList = [
   {
     date: 'วันนี้',
     data: [
       {
+        type: 'consistance',
         title: 'การประเมินความสอดคล้อง',
         content: 'พ.ร.บ.พลังงานนิวเคลียร์',
-        time: '13.00 น.'
+        time: '13.00',
+        readStatus: false
       },
       {
+        type: 'approve',
         title: 'ผู้บริหารอนุมัติ',
         content: 'พ.ร.บ.พลังงานนิวเคลียร์',
-        time: '13.00 น.'
+        time: '13.00',
+        readStatus: false
       },
     ]
   },
@@ -22,14 +22,18 @@ export const NotifyList = [
     date: '2 มี.ค. 2565',
     data: [
       {
+        type: 'consistance',
         title: 'การประเมินความสอดคล้อง',
         content: 'พ.ร.บ.ความปลอดภัย 2554',
-        time: '13.00 น.'
+        time: '09.00',
+        readStatus: false
       },
       {
+        type: 'approve',
         title: 'ผู้บริหารอนุมัติ',
         content: 'พ.ร.บ.ความปลอดภัย 2554',
-        time: '13.00 น.'
+        time: '13.00',
+        readStatus: true
       }
     ]
   },
@@ -37,10 +41,55 @@ export const NotifyList = [
     date: '1 มี.ค. 2565',
     data: [
       {
+        type: 'relevant',
         title: 'การประเมินความเกี่ยวข้อง',
         content: 'พ.ร.บ.ความปลอดภัย 2554',
-        time: '13.00 น.'
+        time: '13.00',
+        readStatus: true
       }
     ]
   }
 ]
+
+export const NotifyIcon = {
+  relevant: {
+    bgColor: '#FFFADE',
+    iconColor: '#FFC700',
+    iconName: 'text-box-search-outline',
+    iconComponent: 'MaterialCommunity'
+  },
+  consistance: {
+    bgColor: '#FFEEDE',
+    iconColor: '#FF9C40',
+    iconName: 'text-box-search-outline',
+    iconComponent: 'MaterialCommunity'
+  },
+  approve: {
+    bgColor: '#DEF4EC',
+    iconColor: '#13AF82',
+    iconName: 'check',
+    iconComponent: 'SimpleLine'
+  }
+}
+
+export const getIconBgColor = (notiType: string) => {
+  switch(notiType) {
+    case 'relevant':
+      return NotifyIcon.relevant.bgColor
+    case 'consistance':
+      return NotifyIcon.consistance.bgColor
+    case 'approve':
+      return NotifyIcon.approve.bgColor
+  }
+}
+
+export const getIconColor = (notiType: string) => {
+  switch(notiType) {
+    case 'relevant':
+      return NotifyIcon.relevant.iconColor
+    case 'consistance':
+      return NotifyIcon.consistance.iconColor
+    case 'approve':
+      return NotifyIcon.approve.iconColor
+  }
+}
--- screens/NotificationScreen.tsx
+++ screens/NotificationScreen.tsx
@@ -1,49 +1,74 @@
 import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import { Octicons, SimpleLineIcons } from '@expo/vector-icons';
+import { SimpleLineIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Colors from '../constants/Colors';
 import { Text, View } from '../components/Themed';
-import { NotifyList } from '../constants/Notification';
+import { NotifyContentModel } from '../model/Notification';
+import { NotifyList, getIconBgColor, getIconColor } from '../constants/Notification';
 
 export default function NotificationScreen({ path }: { path: string }) {
   const [notifyList, setNotifyList] = useState(NotifyList)
 
-  const ContentElement = (contentItem: {title: string, content: string, time: string}, i: number) => {
+  const ContentElement = (contentItem: NotifyContentModel, i: number) => {
     return(
       <View key={'content' + i}>
-        <View style={styles.ContentContainer}>
-          <View style={{ borderRadius: 50, backgroundColor: '#def4ec', paddingHorizontal: 7, paddingVertical: 5 }}>
-            <SimpleLineIcons name="check" size={30} color="#13af82" />
+        <TouchableOpacity>
+          <View style={styles.ContentContainer}>
+            {/* icon */}
+            <View style={styles.IconWrapper}>
+              <View style={{ borderRadius: 60, backgroundColor: getIconBgColor(contentItem.type), padding: 8 }}>
+                {
+                  (contentItem.type == 'relevant' || contentItem.type == 'consistance')
+                  ? <MaterialCommunityIcons name="text-box-search-outline" size={32} color={getIconColor(contentItem.type)} />
+                  : <SimpleLineIcons name="check" size={32} color={getIconColor(contentItem.type)} />
+                }
+              </View>
+              { !contentItem.readStatus && <View style={styles.NotityBadge}></View> }
+            </View>
+
+            {/* title & content */}
+            <View style={styles.ContentWrapper}>
+              <Text style={styles.TextHeader}>{ contentItem.title }</Text>
+              <Text style={styles.TextContent}>{ contentItem.content }</Text>
+            </View>
+
+            {/* time */}
+            <View style={styles.TimeWrapper}>
+              <Text style={styles.TimeText}>{ contentItem.time }</Text>
+            </View>
           </View>
-          <View style={styles.ContentWrapper}>
-            <Text style={styles.TextHeader}>{ contentItem.title }</Text>
-            <Text style={styles.TextContext}>{ contentItem.content }</Text>
-          </View>
-        </View>
+        </TouchableOpacity>
       </View>
     )
   }
 
+  const ContentElementList = (contentData: Array<NotifyContentModel>) => {
+    return (
+      contentData.map((content: NotifyContentModel, index: number) => {
+        return ContentElement(content, index)
+      })
+    )
+  }
+
+  const NotifyElement = notifyList.map((item, index) => {
+    return(
+      <View key={index}>
+        {/* date */}
+        <Text style={styles.DateText}>
+          { item.date }
+        </Text>
+
+        {/* content */}
+        { ContentElementList(item.data) }
+      </View>
+    )
+  })
+
   return (
     <View style={styles.Container}>
       <ScrollView contentContainerStyle={{ flexGrow:1 }}>
-        {
-          notifyList.map((item, index) => {
-            return(
-              <View key={index}>
-                <Text style={styles.DateText}>
-                  { item.date }
-                </Text>
-                { 
-                  item.data.map((content, index) => {
-                    return ContentElement(content, index)
-                  })
-                }
-              </View>
-            )
-          })
-        }
+        { NotifyElement }
       </ScrollView>
     </View>
   );
@@ -65,6 +90,13 @@ const styles = StyleSheet.create({
     color: '#4a4a4a',
     fontFamily: 'Mitr_500Medium'
   },
+  TimeText: {
+    fontSize: 15,
+    color: '#6c6c6c',
+    marginTop: 4,
+    justifyContent: 'flex-end',
+    fontFamily: 'Mitr_400Regular'
+  },
   ContentContainer: {
     flexDirection: 'row',
     padding: 10,
@@ -76,12 +108,32 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     backgroundColor: 'transparent'
   },
+  TimeWrapper: {
+    marginLeft: 'auto',
+    marginRight: 2,
+    backgroundColor: 'transparent'
+  },
+  IconWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
   TextHeader: {
     fontSize: 18,
     fontFamily: 'Mitr_500Medium'
   },
-  TextContext: {
+  TextContent: {
     fontSize: 15,
     fontFamily: 'Mitr_400Regular'
+  },
+  NotityBadge: {
+    width: 10,
+    height: 10,
+    backgroundColor: '#ff4f4f',
+    position: 'absolute',
+    top: 5,
+    right: 2,
+    borderRadius: 10/2
   }
 });
