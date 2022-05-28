28a49c07c3180e6c04cdee59fdbe14cf9f41f9d6
Supansa <s6201012620236@email.kmutnb.ac.th>
2022-05-28 23:44:55 +0700

    install font poppins

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useFonts, Mitr_400Regular, Mitr_500Medium, Mitr_600SemiBold, Mitr_700Bold } from '@expo-google-fonts/mitr';
+import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -12,7 +13,7 @@ export default function App() {
   const colorScheme = useColorScheme();
 
   let [fontsLoaded] = useFonts({
-    Mitr_400Regular, Mitr_500Medium, Mitr_600SemiBold, Mitr_700Bold
+    Mitr_400Regular, Mitr_500Medium, Mitr_600SemiBold, Mitr_700Bold, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold
   });
 
   if (!fontsLoaded) {
