4b59c159fd7a56a91a5dbff1ea4731f4a42ef366
hathaipat-p <hathaipat44@hotmail.co.th>
2022-06-16 19:16:54 +0700

    update TabOneScreen

---
+++ constants/Location.ts
@@ -0,0 +1,19 @@
+export const LocationList = [
+    {
+      location: 'โครงการร้อยเพชร',
+      business: 'food',
+      system: ['CPF SHE', 'OSHAS 18001']
+    },
+    {
+      location: 'โรงงานผลิตอาหารสัตว์บกปักธงชัย',
+      business: 'feed',
+      system: ['CPF SHE', 'ISO 14001']
+    },
+  ]
+  
+  export const LocationIcon = {
+    bgColor: '#C1C1C1',
+    iconColor: '#6C6C6C',
+    iconName: 'menu-right',
+    iconComponent: 'MaterialCommunity'
+  }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,88 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { MaterialCommunityIcons} from '@expo/vector-icons';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { LocationList, LocationIcon } from '../constants/Location';
+import { LocationContentModel } from '../model/Location';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'Home'>) {
+  const [locationList, setNocation] = useState(LocationList)
+
+  const LocationElement = locationList.map((LocationContentModel, index) => {
+    return(
+      <View key={index} >
+        <TouchableOpacity>
+          <View style={styles.ContentContainer}>
+            {/* icon */}
+            <View style={styles.IconWrapper}>
+              <MaterialCommunityIcons name="map-marker-radius-outline" size={32} color={LocationIcon.iconColor} />
+            </View>
+
+            {/* title & content */}
+            <View style={styles.ContentWrapper}>
+              <Text style={styles.TextHeader}>{ LocationContentModel.location }</Text>
+              <Text style={styles.TextContent}>{ LocationContentModel.business }</Text>
+            </View>
+
+            {/* icon */}
+            <View style={styles.MoreWrapper}>
+              <MaterialCommunityIcons name="menu-right" size={32} color={LocationIcon.iconColor} />
+            </View>
+          </View> 
+        </TouchableOpacity>
+      </View>
+    )
+  })
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style={styles.Container}>
+      <ScrollView contentContainerStyle={{ flexGrow:1 }}>
+        { LocationElement }
+      </ScrollView>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     flex: 1,
-    alignItems: 'center',
+    flexGrow:1,
+    paddingTop: 90,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  ContentContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 10
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  ContentWrapper: {
+    marginLeft: 10,
+    backgroundColor: 'transparent'
+  },
+  IconWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  TextHeader: {
+    fontSize: 18,
+    fontFamily: 'Mitr_500Medium'
+  },
+  TextContent: {
+    fontSize: 15,
+    fontFamily: 'Mitr_400Regular'
+  },
+  MoreWrapper: {
+    marginLeft: 'auto',
+    marginRight: 2,
+    backgroundColor: 'transparent'
   },
 });
